4d4a9f13f63cd142bca006819c5b7c2de95cfd32
Brandon <98brandon7@gmail.com>
2022-05-10 20:10:25 -0400

    promblem 02

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,14 +16,24 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(arr, cb) {
+let array = [];
+for (let i = 0; i < arr.length; i++) {
+   array.push(cb(arr[i]));
+}
+return array;
 };
 
 
 
 
 
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
 
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = myMap;
